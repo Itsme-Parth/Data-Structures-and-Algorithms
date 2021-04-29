ef6965b8d213451a195d412ff4fe2f4058082c68
Parth Chadha <parthc2001@gmail.com>
2021-04-29 23:31:01 +0530

    Solutions Added

---
+++ SearchingAndSorting/countRotations.cpp
@@ -0,0 +1,37 @@
+/* 
+This implementation is for right side rotated array where we return mid 
+whereas in left side rotated array we return (n - mid)%n 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int countRotations(int arr[], int n)
+{
+    int start = 0, end = n - 1, count = 0;
+    if (arr[start] <= arr[end])
+        return count;
+    while (start <= end)
+    {
+        if (arr[start] <= arr[end])
+        {
+            return start;
+        }
+        int mid = start + (end - start) / 2;
+        //writing circular array type implementation
+        int next = (mid + n) % n;     //for the last index
+        int prev = (mid + n - 1) % n; //for the first index
+        if (arr[mid] <= arr[prev] && arr[mid] <= arr[next])
+            return mid;
+        else if (arr[start] <= arr[mid])
+            start = mid + 1;
+        else if (arr[mid] <= arr[end])
+            end = mid - 1;
+    }
+}
+int main()
+{
+    int arr[] = {6, 8, 11, 2, 5};
+    int n = sizeof(arr) / sizeof(int);
+    int count = countRotations(arr, n);
+    cout << count;
+    return 0;
+}
