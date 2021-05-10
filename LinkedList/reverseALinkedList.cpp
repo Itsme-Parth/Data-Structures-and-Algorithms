72e800febc4e675a25ac02c045fa21a491a180ad
Parth Chadha <parthc2001@gmail.com>
2021-05-10 23:20:38 +0530

    Solutions Added

---
+++ LinkedList/reverseALinkedList.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+    node* next;
+    int data;
+};
+node* head = NULL;
+void reverse()
+{
+
+}
+//inserting node from the back 
+void insertNode()
+{
+
+}
+void printLinkedList()
+{
+
+}
+void deleteNode()
+{
+
+}
+int main()
+{
+    
+    return 0;
+}
