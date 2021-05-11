237ec0381bde5734df1538b2b686900df9e3fd5d
Parth Chadha <parthc2001@gmail.com>
2021-05-11 22:53:20 +0530

    Solutions Added

--- LinkedList/reverseALinkedList.cpp
+++ LinkedList/reverseALinkedList.cpp
@@ -62,8 +62,8 @@ int main()
     insertNode(4);//list --> 4 5
     insertNode(3);//list --> 3 4 5
     insertNode(2);//list --> 2 3 4 5
-    insertNode(1);
-    insertNode(0);
+    insertNode(1);//list --> 1 2 3 4 5
+    insertNode(0);//list --> 0 1 2 3 4 5
     printLinkedList();
     deleteNode(1);
     printLinkedList();
