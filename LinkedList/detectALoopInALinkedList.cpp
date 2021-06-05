c8be827e2fe107e8254d6de93e352801f8b5b1aa
Parth Chadha <parthc2001@gmail.com>
2021-06-05 22:19:30 +0530

    Solutions Added

--- LinkedList/detectALoopInALinkedList.cpp
+++ LinkedList/detectALoopInALinkedList.cpp
@@ -1,29 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node
-{
-    int data;
-    Node *next;
-
-    Node(int val)
-    {
-        data = val;
-        next = NULL;
-    }
-};
-
-void loopHere(Node *head, Node *tail, int position)
-{
-    if (position == 0)
-        return;
-
-    Node *walk = head;
-    for (int i = 1; i < position; i++)
-        walk = walk->next;
-    tail->next = walk;
-}
-
 class Solution
 {
 public:
@@ -33,44 +7,11 @@ public:
         Node *slow = head, *fast = head;
         while (slow && fast && fast->next)
         {
-            slow = slow->next;
-            fast = fast->next->next;
+            slow = slow->next;       //slow pointer
+            fast = fast->next->next; //fast pointer moves twice as fast as the slow pointer
             if (slow == fast)
                 return true;
         }
         return false;
     }
 };
-
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n, num;
-        cin >> n;
-
-        Node *head, *tail;
-        cin >> num;
-        head = tail = new Node(num);
-
-        for (int i = 0; i < n - 1; i++)
-        {
-            cin >> num;
-            tail->next = new Node(num);
-            tail = tail->next;
-        }
-
-        int pos;
-        cin >> pos;
-        loopHere(head, tail, pos);
-
-        Solution ob;
-        if (ob.detectLoop(head))
-            cout << "True\n";
-        else
-            cout << "False\n";
-    }
-    return 0;
-}
