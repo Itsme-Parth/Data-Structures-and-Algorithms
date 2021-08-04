c198cef938d547494f00d775d6b240eac1740b54
Parth Chadha <parthc2001@gmail.com>
2021-08-04 23:54:15 +0530

    (Edit): rodCuttingProblem added

--- Dynamic Programming/rodCuttingProblem1.cpp
+++ Dynamic Programming/rodCuttingProblem1.cpp
@@ -3,13 +3,14 @@ class Solution
 public:
     int cutRod(int price[], int n)
     {
-        int len[n];
-        for (int i = 0; i < n; i++)
+        int len = n; //len represents the numbers of cut options available in this question len = n
+        int lengthy[len];
+        for (int i = 0; i < len; i++) //creating a lengthy array which holds the size of every cut possible i.e 1 - n
         {
-            len[i] = i + 1;
+            lengthy[i] = i + 1;
         }
-        int dp[n + 1][n + 1];
-        for (int i = 0; i < n + 1; i++)
+        int dp[len + 1][n + 1]; //creating dp array
+        for (int i = 0; i < len + 1; i++)
         {
             dp[i][0] = 0;
         }
@@ -17,21 +18,19 @@ public:
         {
             dp[0][j] = 0;
         }
-        for (int i = 1; i < n + 1; i++)
+        for (int i = 1; i < len + 1; i++) //traverse in rows/i represents the size of the cut
         {
-            for (int j = 1; j < n + 1; j++)
+            for (int j = 1; j < n + 1; j++) //traverse in cols/j represents the length of the rod
             {
-                if (len[i - 1] <= j)
+                if (lengthy[i - 1] <= j)
                 {
-                    int price1 = price[i - 1] + dp[i][j - len[i - 1]];
+                    int price1 = price[i - 1] + dp[i][j - lengthy[i - 1]]; //dp[i] and not [i-1] as we can process again and again
                     int price2 = dp[i - 1][j];
-
                     dp[i][j] = max(price1, price2);
                 }
                 else
                     dp[i][j] = dp[i - 1][j];
             }
         }
-        return dp[n][n];
+        return dp[len][n];
     }
-};
