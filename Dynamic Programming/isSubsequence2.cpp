f49ba81ec688e1d0a0f483fe6bc671212ad93427
Parth Chadha <parthc2001@gmail.com>
2021-08-06 23:28:15 +0530

    (Edit): IsSubsequence DP approach

---
+++ Dynamic Programming/isSubsequence2.cpp
@@ -0,0 +1,35 @@
+/* Given two strings s and t, return true if s is a subsequence of t, or false otherwise. */
+//O(n*m) approach using DP
+class Solution
+{
+public:
+    bool isSubsequence(string s, string t)
+    {
+        int n = s.length();
+        int m = t.length();
+        int dp[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int j = 0; j < m + 1; j++)
+            {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+        for (int i = 1; i < n + 1; i++)
+        {
+            for (int j = 1; j < m + 1; j++)
+            {
+                if (s[i - 1] == t[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        // cout << n << " " << dp[n][m];
+        if (n == dp[n][m])
+            return true;
+        else
+            return false;
+    }
+};
