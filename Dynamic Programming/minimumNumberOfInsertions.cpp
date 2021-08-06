b9c2cdb30781cb2d3237fcb7d1e8eeab1456cb6d
Parth Chadha <parthc2001@gmail.com>
2021-08-06 23:29:06 +0530

    (Edit): minimum number of insertions

---
+++ Dynamic Programming/minimumNumberOfInsertions.cpp
@@ -0,0 +1,31 @@
+class Solution{
+  public:
+    int countMin(string str){
+    //complete the function here
+    string strRev = string(str.rbegin(), str.rend());
+        int n = str.length();
+        int dp[n + 1][n + 1];
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int j = 0; j < n + 1; j++)
+            {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+        for (int i = 1; i < n + 1; i++)
+        {
+            for (int j = 1; j < n + 1; j++)
+            {
+                if (str[i - 1] == strRev[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        //now if we found the coomon elements then all we need to do is
+        //insert the left elements, so that we can pairs and the string becomes
+        //a palindrome
+        return n - dp[n][n];
+    }
+};
