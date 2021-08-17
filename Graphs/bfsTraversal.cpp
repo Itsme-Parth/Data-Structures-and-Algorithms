f6212cd55049d866f2e5092b8bace92edfb016e1
Parth Chadha <parthc2001@gmail.com>
2021-08-17 23:46:31 +0530

    Add: graph bfsTraversal

---
+++ Graphs/bfsTraversal.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    //Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[])
+    {
+        vector<int> bfs;       //bfs vector
+        vector<int> vis(V, 0); //visited vector with all nodes initialised to 0
+        queue<int> q;          //queue to help bfs implementation
+        q.push(0);             //pushing the first node
+        vis[0] = 1;
+        while (!q.empty())
+        {
+            int node = q.front(); //select the top node
+            q.pop();
+            bfs.push_back(node);       //push the current node to result
+            for (auto itr : adj[node]) //check neighbours of the current node
+            {
+                if (!vis[itr]) //add in the queue if not visited
+                {
+                    q.push(itr);
+                    vis[itr] = 1;
+                }
+            }
+        }
+        return bfs;
+    }
+};
