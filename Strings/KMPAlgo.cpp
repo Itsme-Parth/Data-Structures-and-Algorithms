45a70765c1a905af56e99c507c0f7c46f3fd61f7
Parth Chadha <parthc2001@gmail.com>
2021-08-24 23:31:53 +0530

    Add: String Solutions

--- Strings/KMPAlgo.cpp
+++ Strings/KMPAlgo.cpp
@@ -1,11 +1,9 @@
-// C++ program for implementation of KMP pattern searching
-// algorithm
 #include <bits/stdc++.h>
 
-void computeLPSArray(char* pat, int M, int* lps);
+void computeLPSArray(char *pat, int M, int *lps);
 
 // Prints occurrences of txt[] in pat[]
-void KMPSearch(char* pat, char* txt)
+void KMPSearch(char *pat, char *txt)
 {
 	int M = strlen(pat);
 	int N = strlen(txt);
@@ -19,67 +17,34 @@ void KMPSearch(char* pat, char* txt)
 
 	int i = 0; // index for txt[]
 	int j = 0; // index for pat[]
-	while (i < N) {
-		if (pat[j] == txt[i]) {
-			j++;
+	while (i < N)
+	{
+		if (pat[i] == pat[j])
+		{
 			i++;
+			j++;
 		}
-
-		if (j == M) {
-			printf("Found pattern at index %d ", i - j);
+		if (j == M)
+		{
+			cout << "pattern exists at " << i - j;
 			j = lps[j - 1];
 		}
-
-		// mismatch after j matches
-		else if (i < N && pat[j] != txt[i]) {
-			// Do not match lps[0..lps[j-1]] characters,
-			// they will match anyway
-			if (j != 0)
-				j = lps[j - 1];
-			else
-				i = i + 1;
-		}
-	}
-}
-
-// Fills lps[] for given patttern pat[0..M-1]
-void computeLPSArray(char* pat, int M, int* lps)
-{
-	// length of the previous longest prefix suffix
-	int len = 0;
-
-	lps[0] = 0; // lps[0] is always 0
-
-	// the loop calculates lps[i] for i = 1 to M-1
-	int i = 1;
-	while (i < M) {
-		if (pat[i] == pat[len]) {
-			len++;
-			lps[i] = len;
-			i++;
-		}
-		else // (pat[i] != pat[len])
+		else if (i < N && pat[j] != txt[i])
 		{
-			// This is tricky. Consider the example.
-			// AAACAAAA and i = 7. The idea is similar
-			// to search step.
-			if (len != 0) {
-				len = lps[len - 1];
-
-				// Also, note that we do not increment
-				// i here
-			}
-			else // if (len == 0)
+			if (j != 0)
 			{
-				lps[i] = 0;
-				i++;
+				j = lps[j - 1];
 			}
+			else
+				i++;
 		}
 	}
 }
+void calculateRes(string s, int length, vector<int> &res)
+	/* check your own implementation of calculateLps file for this */
 
-// Driver program to test above function
-int main()
+	// Driver program to test above function
+	int main()
 {
 	char txt[] = "ABABDABACDABABCABAB";
 	char pat[] = "ABABCABAB";
