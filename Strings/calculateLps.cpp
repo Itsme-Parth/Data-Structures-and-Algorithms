830ed9edff77816f4e672ac735562ca526b7d863
Parth Chadha <parthc2001@gmail.com>
2021-08-24 23:32:08 +0530

    Add: String Solutions

---
+++ Strings/calculateLps.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    void calculateRes(string s, int length, vector<int> &res)
+    {
+        int len = 0;
+        res[len] = 0;
+        int i = 1;
+        while (i < length)
+        {
+            if (s[i] == s[len])
+            {
+                len++;
+                res[i] = len;
+                i++;
+            }
+            else //if s[i] != s[len]
+            {
+                if (len != 0) //if len != 0 Consider case AAACAAAA
+                    len = res[len - 1];
+                else
+                {
+                    res[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+public:
+    int lps(string s)
+    {
+        int n = s.size();
+        vector<int> res(n);
+        calculateRes(s, n, res);
+        return *max_element(res.begin(), res.end());
+    }
+};
