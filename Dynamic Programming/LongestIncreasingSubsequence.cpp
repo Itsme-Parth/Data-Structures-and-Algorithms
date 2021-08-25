978ce9cecd171f1a46a2ab4d10f088f174656c9e
Parth Chadha <parthc2001@gmail.com>
2021-08-25 17:55:05 +0530

    Add: Longest Increasing Subsequence

--- Dynamic Programming/LongestIncreasingSubsequence.cpp
+++ Dynamic Programming/LongestIncreasingSubsequence.cpp
@@ -4,7 +4,7 @@ public:
     //Function to find length of longest increasing subsequence.
     int longestSubsequence(int n, int arr[])
     {
-        vector<int> dp(n, 1);
+        vector<int> dp(n, 1); //ek kitna minimum bna skta
         for (int i = 1; i < n; i++)
         {
             for (int j = 0; j < i; j++)
@@ -12,7 +12,7 @@ public:
                 if (arr[j] < arr[i]) //if j element smaller than current i element
                 {
                     dp[i] = max(dp[i], dp[j] + 1); //curr ya jo chote element wala hai usme + 1
-                }
+                }                                  //dp[j] ki updated value mai hi 1 add karenge
             }
         }
         return *max_element(dp.begin(), dp.end());
