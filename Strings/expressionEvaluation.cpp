6e2d50126f5a5ccbbf082c01aa8f51d15f5f57c4
Parth Chadha <parthc2001@gmail.com>
2021-08-25 19:58:48 +0530

    Update: Expression Evaluation

--- Strings/expressionEvaluation.cpp
+++ Strings/expressionEvaluation.cpp
@@ -1,165 +1,49 @@
-// CPP program to evaluate a given
-// expression where tokens are
-// separated by space.
-#include <bits/stdc++.h>
-using namespace std;
-
-// Function to find precedence of
-// operators.
-int precedence(char op)
+class Solution
 {
-    if (op == '+' || op == '-')
-        return 1;
-    if (op == '*' || op == '/')
-        return 2;
-    return 0;
-}
-
-// Function to perform arithmetic operations.
-int applyOp(int a, int b, char op)
-{
-    switch (op)
+public:
+    int calculate(string s)
     {
-    case '+':
-        return a + b;
-    case '-':
-        return a - b;
-    case '*':
-        return a * b;
-    case '/':
-        return a / b;
-    }
-}
+        int result = 0;
+        int num = 0, sign = 1;
+        stack<int> nums, ops;
 
-// Function that returns value of
-// expression after evaluation.
-int evaluate(string tokens)
-{
-    int i;
-
-    // stack to store integer values.
-    stack<int> values;
-
-    // stack to store operators.
-    stack<char> ops;
-
-    for (i = 0; i < tokens.length(); i++)
-    {
-
-        // Current token is a whitespace,
-        // skip it.
-        if (tokens[i] == ' ')
-            continue;
-
-        // Current token is an opening
-        // brace, push it to 'ops'
-        else if (tokens[i] == '(')
+        for (int i = 0; i < s.size(); ++i)
         {
-            ops.push(tokens[i]);
-        }
-
-        // Current token is a number, push
-        // it to stack for numbers.
-        else if (isdigit(tokens[i]))
-        {
-            int val = 0;
-
-            // There may be more than one
-            // digits in number.
-            while (i < tokens.length() &&
-                   isdigit(tokens[i]))
+            char c = s[i];
+            if (c >= '0' && c <= '9')
             {
-                val = (val * 10) + (tokens[i] - '0');
-                i++;
+                num = 10 * num + c - '0'; /// For case: "23"
             }
-
-            values.push(val);
-
-            // right now the i points to
-            // the character next to the digit,
-            // since the for loop also increases
-            // the i, we would skip one
-            //  token position; we need to
-            // decrease the value of i by 1 to
-            // correct the offset.
-            i--;
-        }
-
-        // Closing brace encountered, solve
-        // entire brace.
-        else if (tokens[i] == ')')
-        {
-            while (!ops.empty() && ops.top() != '(')
+            else if (c == '+')
             {
-                int val2 = values.top();
-                values.pop();
-
-                int val1 = values.top();
-                values.pop();
-
-                char op = ops.top();
-                ops.pop();
-
-                values.push(applyOp(val1, val2, op));
+                result += num * sign; /// everytime meets an operator, sum up previous number
+                num = 0;
+                sign = 1; /// sign is the sign of next number
             }
-
-            // pop opening brace.
-            if (!ops.empty())
-                ops.pop();
-        }
-
-        // Current token is an operator.
-        else
-        {
-            // While top of 'ops' has same or greater
-            // precedence to current token, which
-            // is an operator. Apply operator on top
-            // of 'ops' to top two elements in values stack.
-            while (!ops.empty() && precedence(ops.top()) >= precedence(tokens[i]))
+            else if (c == '-')
             {
-                int val2 = values.top();
-                values.pop();
-
-                int val1 = values.top();
-                values.pop();
-
-                char op = ops.top();
-                ops.pop();
-
-                values.push(applyOp(val1, val2, op));
+                result += num * sign; /// everytime meets an operator, sum up previous number
+                num = 0;
+                sign = -1;
             }
+            else if (c == '(')
+            {
+                nums.push(result); /// ...(1+3+(..xx..)+...)... before go into cur (..xx..), record the forefront result (in this case it is 1+3 ) into nums array
+                ops.push(sign);    // record cur (..xx..) sign
+                result = 0;        // result is to record the total value in the cur (..xx..)
+                sign = 1;
+            }
+            else if (c == ')' && ops.size())
+            {
+                result += num * sign; /// For case: 1-(5)
+                num = 0;
 
-            // Push current token to 'ops'.
-            ops.push(tokens[i]);
+                result = result * ops.top() + nums.top(); // ...(1+3+(..xx..)+...)... sum up cur (..xx..)  and the forefront result (in this case it is 1+3 )
+                nums.pop();
+                ops.pop();
+            }
         }
+        result += num * sign; /// For the last one operation. consider the case:  1+2+3
+        return result;
     }
-
-    // Entire expression has been parsed at this
-    // point, apply remaining ops to remaining
-    // values.
-    while (!ops.empty())
-    {
-        int val2 = values.top();
-        values.pop();
-
-        int val1 = values.top();
-        values.pop();
-
-        char op = ops.top();
-        ops.pop();
-
-        values.push(applyOp(val1, val2, op));
-    }
-
-    // Top of 'values' contains result, return it.
-    return values.top();
-}
-
-int main()
-{
-    cout << evaluate("10 + 2 * 6") << "\n";
-    cout << evaluate("100 * 2 + 12") << "\n";
-    cout << evaluate("100 * ( 2 + 12 )") << "\n";
-    cout << evaluate("100 * ( 2 + 12 ) / 14");
-    return 0;
-}
+};
