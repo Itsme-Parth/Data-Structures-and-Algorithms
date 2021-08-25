191b15c8e73cb763e28df82cc5c726f7ffb453cb
Parth Chadha <parthc2001@gmail.com>
2021-08-25 17:57:31 +0530

    Add: Peak Element

---
+++ SearchingAndSorting/Binary Search Questions/peakElements.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &nums)
+    {
+        if (nums.size() == 1)
+            return 0; // single element
+
+        int n = nums.size();
+
+        // check if 0th/n-1th index is the peak element
+        if (nums[0] > nums[1])
+            return 0;
+        if (nums[n - 1] > nums[n - 2])
+            return n - 1;
+
+        // search in the remaining array
+        int start = 1;
+        int end = n - 2;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            //if it's greater than it's neighbours then peak element
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return mid;
+            //we move to the more promising side i.e. which among the neighbours has a greater value
+            else if (nums[mid] < nums[mid - 1])
+                end = mid - 1;
+            else if (nums[mid] < nums[mid + 1])
+                start = mid + 1;
+        }
+        return -1; // dummy return statement
+    }
+};
